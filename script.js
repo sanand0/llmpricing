c4f30d7226f9824a20b1246fc871bd41d5ccd8e0
S Anand <root.node@gmail.com>
2024-05-10 10:15:45 +0800

    ENH: Update cost, automate optimal calculation

--- script.js
+++ script.js
@@ -6,20 +6,36 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 const content = await fetch("README.md").then((r) => r.text());
 document.querySelector("#README").innerHTML = marked.parse(content);
 
-const data = (await d3.csv("elo.csv")).filter((d) => d.cpmi);
-data.forEach((d) => {
+const models = (await d3.csv("elo.csv")).filter((d) => d.cpmi);
+models.forEach((d) => {
   d.cost = +d.cpmi;
   d.elo = +d.elo;
 });
+// model.optimal = "best" if no model has higher ELO and lower cost
+// model.optimal = "worst" if no model has lower ELO and higher cost
+models.forEach((model) => {
+  let isBest = true;
+  let isWorst = true;
+  models.forEach((other) => {
+    if (model === other) return;
+    // If other model has higher ELO and lower cost, this model is not best
+    if (other.elo >= model.elo && other.cost <= model.cost) isBest = false;
+    // If other model has lower ELO and higher cost, this model is not worst
+    if (other.elo <= model.elo && other.cost >= model.cost) isWorst = false;
+  });
+  model.optimal = isBest ? "best" : isWorst ? "worst" : "";
+});
+
 
 document.querySelector("#llm-cost").replaceChildren(
   Plot.plot({
+    marginLeft: 50,
     x: { type: "log", grid: true },
     y: { grid: true },
     width: 1000,
     height: 500,
     marks: [
-      Plot.dot(data, {
+      Plot.dot(models, {
         x: "cost",
         y: "elo",
         r: 8,
@@ -38,7 +54,7 @@ document.querySelector("#llm-cost").replaceChildren(
         },
       }),
       Plot.text(
-        data.filter((d) => d.optimal),
+        models.filter((d) => d.optimal),
         {
           x: "cost",
           y: "elo",
@@ -52,6 +68,7 @@ document.querySelector("#llm-cost").replaceChildren(
       }),
       Plot.axisY({
         label: "ELO score",
+        tickSpacing: 100,
       }),
     ],
   }),
